4f3a685a2f891993fc55c47e611e4440828f3530
Kovrolin Nedorogo <sbst@yandex.ru>
2024-12-27 15:35:59 +0100

    Read file in blocks

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
+	"io"
+	"math"
 	"os"
 )
 
@@ -12,15 +14,52 @@ func check(e error) {
 	}
 }
 
-func main() {
-	f, e := os.Open("../dat")
-	check(e)
-	block := make([]byte, 1024*1024)
-	_, e = f.Read(block)
+const blockSize int64 = 1024 * 1024
+
+func read(fileName string, startBlockId int64, nrBlocks int64, result []string) {
+	f, e := os.Open(fileName)
 	check(e)
 	hasher := sha256.New()
-	_, e = hasher.Write(block)
+	curBlockSize := blockSize
+	block := make([]byte, curBlockSize)
+	curBlockId := startBlockId
+	for nrBlocks > 0 {
+		n, e := f.Read(block)
+		if n == 0 && e == io.EOF {
+			break
+		}
+		check(e)
+		if int64(n) < blockSize {
+			curBlockSize = int64(n)
+			block = block[0:curBlockSize]
+		}
+		if int64(n) > curBlockSize {
+			panic("Unexpected read block size")
+		}
+		_, e = hasher.Write(block)
+		check(e)
+		sum := hasher.Sum(nil)
+		result[curBlockId] = fmt.Sprintf("%x", sum)
+		hasher.Reset()
+		curBlockId += 1
+		nrBlocks -= 1
+	}
+	e = f.Close()
 	check(e)
-	sum := hasher.Sum(nil)
-	fmt.Printf("%x\n", sum)
+}
+
+func main() {
+	fileName := "../dat2"
+	f, e := os.Open(fileName)
+	check(e)
+	info, e := f.Stat()
+	check(e)
+	e = f.Close()
+	check(e)
+	nrBlocks := int64(math.Ceil(float64(info.Size()) / float64(blockSize)))
+	hashes := make([]string, nrBlocks)
+	read(fileName, 0, nrBlocks, hashes)
+	for _, hash := range hashes {
+		fmt.Println(hash)
+	}
 }
