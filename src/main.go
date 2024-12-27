5524a3c97337caac3438af8ad9caed1412b97fde
Kovrolin Nedorogo <sbst@yandex.ru>
2024-12-27 21:16:30 +0100

    Add multithreaded read

--- src/main.go
+++ src/main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"math"
+	"os"
+	"sync"
 )
 
 func check(e error) {
@@ -13,13 +15,25 @@ func check(e error) {
 	}
 }
 
-const blockSize int64 = 1024 * 1024
-
-func getNrOfBlocks(size int64) int64 {
+func getNrOfBlocks(size int64, blockSize int64) int64 {
 	return int64(math.Ceil(float64(size) / float64(blockSize)))
 }
 
-func read(reader io.Reader, startBlockId int64, nrBlocks int64, result []string) {
+func getFileSize(fileName string) int64 {
+	f, e := os.Open(fileName)
+	check(e)
+	info, e := f.Stat()
+	check(e)
+	e = f.Close()
+	check(e)
+	return info.Size()
+}
+
+func skipBlocks(file *os.File, blockSize int64, nrOfBlocks int64) {
+	file.Seek(blockSize*nrOfBlocks, io.SeekStart)
+}
+
+func read(reader io.Reader, blockSize int64, startBlockId int64, nrBlocks int64, result []string) {
 	hasher := sha256.New()
 	block := make([]byte, blockSize)
 	curBlockId := startBlockId
@@ -39,6 +53,30 @@ func read(reader io.Reader, startBlockId int64, nrBlocks int64, result []string)
 		curBlockId += 1
 		nrBlocks -= 1
 	}
+}
+
+func readFileExec(fileName string, blockSize int64, startBlockId int64, nrBlocks int64, wait *sync.WaitGroup, result []string) {
+	f, e := os.Open(fileName)
+	skipBlocks(f, blockSize, startBlockId)
+	check(e)
+	read(f, blockSize, startBlockId, nrBlocks, result)
+	e = f.Close()
+	check(e)
+	wait.Done()
+}
+
+func readFile(fileName string, blockSize int64, nrBlocksPerThread int64) []string {
+	var wait = sync.WaitGroup{}
+	nrBlocksTotal := getNrOfBlocks(getFileSize(fileName), blockSize)
+	hashes := make([]string, nrBlocksTotal)
+	var processedBlocks int64 = 0
+	for processedBlocks < nrBlocksTotal {
+		wait.Add(1)
+		go readFileExec(fileName, blockSize, processedBlocks, nrBlocksPerThread, &wait, hashes)
+		processedBlocks += nrBlocksPerThread
+	}
+	wait.Wait()
+	return hashes
 }
 
 func main() {
--- src/main_test.go
+++ src/main_test.go
@@ -1,20 +1,91 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"strings"
 	"testing"
 )
 
-func getFileSize(file *os.File) int64 {
-	info, e := file.Stat()
-	check(e)
-	return info.Size()
+// func readLoad(nrOfBlocksPerThread int64) string {
+// 	numThreads = 0
+// 	fileName := "../../datbig"
+// 	start := time.Now()
+// 	hashes := readFile(fileName, 1024, nrOfBlocksPerThread)
+// 	elapsed := time.Since(start)
+// 	createdThreads := numThreads
+// 	numThreads = 0
+// 	return fmt.Sprintf("\nNumber of blocks: %v\nBlocks per thread: %v\nCreated threads: %v\nElapsed: %.3fs\n\n", len(hashes), nrOfBlocksPerThread, createdThreads, elapsed.Seconds())
+// }
+
+// func TestLoad(t *testing.T) {
+// 	var nrBlocks int64 = 512000 * 2
+// 	var msg string
+// 	for nrBlocks != 1 {
+// 		msg += readLoad(nrBlocks)
+// 		nrBlocks = nrBlocks / 2
+// 	}
+// 	t.Log(msg)
+// }
+
+func isEqual(hashes1 []string, hashes2 []string) (bool, string) {
+	if len(hashes1) != len(hashes2) {
+		return false, fmt.Sprintf("Hash size array: %v, expected: %v ", len(hashes1), len(hashes2))
+	}
+
+	for i := range hashes1 {
+		if hashes1[i] != hashes2[i] {
+			return false, fmt.Sprintf("Hash for %v block: %v, expected: %v ", i, hashes1[i], hashes2[i])
+		}
+	}
+	return true, ""
 }
 
-func TestReadHalfBlockFullFile(t *testing.T) {
+func TestReadOneByOne(t *testing.T) {
 	fileName := "../../dat2"
-	expected := [...]string{
+	var blockSize int64 = 1024 * 1024
+	expected := []string{
+		"bee2a0ac4cf8f4f77117d3e0be917cac8c600dfb99154018ef3cebe392724298",
+		"0bf0567803b83fa4a4202d939365b7dde052af4086eadb19ca9494cb2cb28bf4",
+		"a554c7573c4760d40b2d391557792d9f6588ee3200fef6ff6b34b7bbcb04659d",
+	}
+
+	f, e := os.Open(fileName)
+	check(e)
+	nrBlocks := 3
+	hashes := make([]string, nrBlocks)
+	read(f, blockSize, 0, 1, hashes)
+	read(f, blockSize, 1, 1, hashes)
+	read(f, blockSize, 2, 1, hashes)
+	e = f.Close()
+	check(e)
+	equal, msg := isEqual(hashes, expected)
+	if !equal {
+		t.Fatal(msg)
+	}
+}
+
+func TestReadSmallBuffer(t *testing.T) {
+	data := "abcde"
+	var blockSize int64 = 1024 * 1024
+	totalBlocks := getNrOfBlocks(int64(len(data)), blockSize)
+	expected := []string{
+		"36bbe50ed96841d10443bcb670d6554f0a34b761be67ec9c4a8ad2c0c44ca42c",
+	}
+
+	hashes := make([]string, totalBlocks)
+	reader := strings.NewReader("abcde")
+	read(reader, blockSize, 0, totalBlocks, hashes)
+	equal, msg := isEqual(hashes, expected)
+	if !equal {
+		t.Fatal(msg)
+	}
+}
+
+func TestReadFile(t *testing.T) {
+	fileName := "../../dat2"
+	var blockSize int64 = 1024 * 1024
+	expected := []string{
 		"bee2a0ac4cf8f4f77117d3e0be917cac8c600dfb99154018ef3cebe392724298",
 		"0bf0567803b83fa4a4202d939365b7dde052af4086eadb19ca9494cb2cb28bf4",
 		"a554c7573c4760d40b2d391557792d9f6588ee3200fef6ff6b34b7bbcb04659d",
@@ -22,68 +93,37 @@ func TestReadHalfBlockFullFile(t *testing.T) {
 		"cf745822a74607477bdb7e7a952941098fd09692c64b7c468d115b1c1a5fdace",
 		"dcf2c240d59f12c3fda471fc54d4451184c90f08981556f61657b51453ab5f7a",
 	}
-
-	f, e := os.Open(fileName)
-	check(e)
-	totalBlocks := getNrOfBlocks(getFileSize(f))
-	hashes := make([]string, totalBlocks)
-	read(f, 0, totalBlocks, hashes)
-	e = f.Close()
-	check(e)
-	if len(hashes) != len(expected) {
-		t.Fatalf("Hash size array: %v, expected: %v ", len(hashes), len(expected))
-	}
-
-	for i := range hashes {
-		if hashes[i] != expected[i] {
-			t.Fatalf("Hash for %v block: %v, expected: %v ", i, hashes[i], expected[i])
-		}
+	hashes := readFile(fileName, blockSize, 2)
+	equal, msg := isEqual(hashes, expected)
+	if !equal {
+		t.Fatal(msg)
 	}
 }
 
-func TestReadFullBlockFullFile(t *testing.T) {
+func TestReadFileFullBlockFullFile(t *testing.T) {
 	fileName := "../../dat"
-	expected := [...]string{
+	var blockSize int64 = 1024 * 1024
+	expected := []string{
 		"460960a020fbad248be7e4c93900bbe36bbd371c7bc61e426d3d7748e34acbdf",
 		"075976462a537d4360fe64ddf3e1ef0dd7db14f003aecc630861b57c22bfa518",
 		"e8f1614c60cb143a301b3fc3a9f039a7c10cca6c8a64cd6f52624fdf06970303",
 	}
 
-	f, e := os.Open(fileName)
-	check(e)
-	totalBlocks := getNrOfBlocks(getFileSize(f))
-	hashes := make([]string, totalBlocks)
-	read(f, 0, totalBlocks, hashes)
-	e = f.Close()
-	check(e)
-	if len(hashes) != len(expected) {
-		t.Fatalf("Hash size array: %v, expected: %v ", len(hashes), len(expected))
-	}
-
-	for i := range hashes {
-		if hashes[i] != expected[i] {
-			t.Fatalf("Hash for %v block: %v, expected: %v ", i, hashes[i], expected[i])
-		}
+	hashes := readFile(fileName, blockSize, 1)
+	equal, msg := isEqual(hashes, expected)
+	if !equal {
+		t.Fatal(msg)
 	}
 }
 
-func TestReadSmallBuffer(t *testing.T) {
-	data := "abcde"
-	totalBlocks := getNrOfBlocks(int64(len(data)))
-	expected := [...]string{
-		"36bbe50ed96841d10443bcb670d6554f0a34b761be67ec9c4a8ad2c0c44ca42c",
-	}
+func TestReadFileMultipleThreads(t *testing.T) {
+	fileName := "../../dat"
+	var blockSize int64 = 1024 * 1024
 
-	hashes := make([]string, totalBlocks)
-	reader := strings.NewReader("abcde")
-	read(reader, 0, totalBlocks, hashes)
-	if len(hashes) != len(expected) {
-		t.Fatalf("Hash size array: %v, expected: %v ", len(hashes), len(expected))
-	}
-
-	for i := range hashes {
-		if hashes[i] != expected[i] {
-			t.Fatalf("Hash for %v block: %v, expected: %v ", i, hashes[i], expected[i])
-		}
+	hashes1 := readFile(fileName, blockSize, 1)
+	hashes2 := readFile(fileName, blockSize, 100)
+	equal, msg := isEqual(hashes1, hashes2)
+	if !equal {
+		t.Fatal(msg)
 	}
 }
